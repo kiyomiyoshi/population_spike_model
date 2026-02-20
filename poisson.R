8a3e9b0f9ba879efc977d76779d2251c11c8262a
kiyomiyoshi <miyoshi80@gmail.com>
2026-02-20 13:25:30 +0900

    commit

--- poisson.R
+++ poisson.R
@@ -16,9 +16,9 @@ colors <- color_wheel()
 n_neurons <- 360                                               # Number of neurons in the population
 orientations <- seq(1, 360, by = 1)                            # Possible orientations (0 to 359 degrees)
 preferred_orientations <-  seq(1, 360, length.out = n_neurons) # Preferred orientation of each neuron
-max_firing_rate <- 50                                          # Maximum firing rate of each neuron (30 or 50)
-tuning_width <- 25                                             # Tuning width (standard deviation) of each neuron's response curve (30, or 50)
-spontaneous_firing_rate <- 0                                   # (0 or 10)
+max_firing_rate <- 60                                          # Maximum firing rate of each neuron
+tuning_width <- 40                                             # Tuning width (standard deviation) of each neuron's response curve
+spontaneous_firing_rate <- 0                                   
 
 # Tuning curves
 tuning_curves <- matrix(0, nrow = n_neurons, ncol = length(orientations))
@@ -48,28 +48,27 @@ responses <- c()
 
 input_90 <- rnorm(1000, 90, 0) # stimulus input plus internal noise
 for (i in 1:1000) {
-  resp <- rpois(n_neurons, lambda = tuning_curves[, input_90[i] + 1]) + spontaneous_firing_rate # important
+  resp <- rpois(n_neurons, lambda = tuning_curves[, input_90[i] + 1]) + spontaneous_firing_rate
   responses <- rbind(responses, cbind(resp, seq(1, 360, 1), 90, i))
 }
 
 input_120 <- rnorm(1000, 120, 0) # stimulus input plus internal noise
 for (i in 1:1000) {
-  resp <- rpois(n_neurons, lambda = tuning_curves[, input_120[i] + 1]) + spontaneous_firing_rate # important
+  resp <- rpois(n_neurons, lambda = tuning_curves[, input_120[i] + 1]) + spontaneous_firing_rate
   responses <- rbind(responses, cbind(resp, seq(1, 360, 1), 120, i))
 }
 
 df_responses_1 <- as.data.frame(responses)
 colnames(df_responses_1) <- c("Spikes", "Neuron", "Stimulus", "Trial")
 
-
 ### Unattended ###
 # Parameters (unattended)
-n_neurons <- 360                                               # Number of neurons in the population
-orientations <- seq(1, 360, by = 1)                            # Possible orientations (0 to 359 degrees)
-preferred_orientations <-  seq(1, 360, length.out = n_neurons) # Preferred orientation of each neuron
-max_firing_rate <- 25                                          # Maximum firing rate of each neuron (30 or 50)
-tuning_width <- 50                                             # Tuning width (standard deviation) of each neuron's response curve (30, or 50)
-spontaneous_firing_rate <- 0                                   # (0 or 10)
+n_neurons <- 360                                               
+orientations <- seq(1, 360, by = 1)                           
+preferred_orientations <-  seq(1, 360, length.out = n_neurons)
+max_firing_rate <- 60 * 0.8                                          
+tuning_width <- 40 * 1.25                                            
+spontaneous_firing_rate <- 0                                  
 
 # Tuning curves
 tuning_curves <- matrix(0, nrow = n_neurons, ncol = length(orientations))
@@ -112,7 +111,7 @@ for (i in 1:1000) {
 df_responses_2 <- as.data.frame(responses)
 colnames(df_responses_2) <- c("Spikes", "Neuron", "Stimulus", "Trial")
 
-###
+### Visualization ###
 p <- cowplot::plot_grid(p1, p2)
 p
 
@@ -128,19 +127,3 @@ df_sum %>%
   group_by(Stimulus, Attention) %>%
   summarise(Mean_sum_spikes = mean(Sum_spikes),
             SD_sum_spikes = sd(Sum_spikes), .groups = "drop") 
-
-
-# Fano factor一定のポアソンモデルを使う限り、総和が同じなら分散も同じになる 
-# Fano factorの実験結果自体があるから単にそれ使えばいい
-
-# 負の二項分布（最もよく使われる）
-# k → ∞ でポアソンに近づく
-# k が小さいほど分散が増える
-
-# Quasi-Poisson モデル（GLMで便利）
-# Fano factor > 1 を自然に表現
-
-# fine grained discriminationだと2次元間が相関する(Rahnev open mindと繋がる)
-# 刺激のばらつきが2つのチューニングカーブの傘下に入る、入らないで正の相関が出る
-# coarse grainedだと相関しない
-# Samahaで総和でvisibilityが決まることは報告されている
